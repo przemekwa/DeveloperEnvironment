212b24f30a08e154b3b51b761456959bc68b50a2
Przemysław Walkowski <przemyslaw.walkowski@bccgroup.com>
2019-01-16 17:18:07 +0100

    CMDER update

--- AppSettings/Cmder/user-aliases.cmd
+++ AppSettings/Cmder/user-aliases.cmd
@@ -6,6 +6,7 @@
 ;= Add aliases below here
 e.=explorer .
 gl=git log --oneline --all --graph --decorate  $*
+glog=git log --after='$*' --pretty=format:%B
 ls=ls --show-control-chars -F --color $*
 pwd=cd
 clear=cls
@@ -14,3 +15,18 @@ unalias=alias /d $1
 vi=vim $*
 cmderr=cd /d "%CMDER_ROOT%"
 np="C:\Program Files\Notepad++\notepad++.exe" $*
+SelgrosCopy= dotnet "D:\Projects\SelgrosCopy\SelgrosCopy\bin\Release\netcoreapp2.1\SelgrosCopy.dll" $*
+npg = cd D:\Projects\Selgros\PG\trunk\
+ib = cd D:\Projects\imagebank\trunk\ImageBank\
+web-config = node "D:\Projects\web_config_changer\webconfchange.js" -c $*
+schema2 = "C:\Program Files\Notepad++\notepad++.exe" "D:\Projects\Selgros\PG\trunk\DBScripts\SelgrosPG_Schema2.sql"
+user = "C:\Program Files\Notepad++\notepad++.exe" "D:\Program Files\cmder\config\user-aliases.cmd"
+selgros = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "D:\Projects\Selgros\PG\trunk\SelgrosPG.sln"
+imagebank = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "D:\Projects\imagebank\trunk\ImageBank\ImageBank.sln"
+zasp = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "D:\Projects\ZASP_APP\ZASP_APP.sln"
+study = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "D:\Projects\ProgramingStudy\ProgramingStudy\ProgramingStudy.sln"
+dev = cd "D:\Projects\DeveloperEnvironment\"
+ssms = "C:\Program Files (x86)\Microsoft SQL Server\140\Tools\Binn\ManagementStudio\Ssms.exe"
+branzapl = sqlcmd -S .\SQLEXPRESS -Q "SELECT BRANZA FROM [SelgrosMainDB].[dbo].[Artykuly] where ARTYKUL=$1 and ZAKLAD=100"
+branzaro = sqlcmd -S .\SQLEXPRESS_RO -Q "SELECT BRANZA FROM [SelgrosMainDB].[dbo].[Artykuly] where ARTYKUL=$1 and ZAKLAD=100"
+branzaru = sqlcmd -S .\SQLEXPRESS_RU -Q "SELECT BRANZA FROM [SelgrosMainDB].[dbo].[Artykuly] where ARTYKUL=$1 and ZAKLAD=0"
